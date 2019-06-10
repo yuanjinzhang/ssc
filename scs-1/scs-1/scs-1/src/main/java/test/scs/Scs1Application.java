0e29d3826d79343d225e7bf1f083ee16dc7705b4
S16204101 <1032982007@qq.com>
2019-06-10 20:00:25 +0800

    scs-1
    系统管理员与机房管理员

---
+++ scs-1/scs-1/scs-1/src/main/java/test/scs/Scs1Application.java
@@ -0,0 +1,13 @@
+package test.scs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Scs1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Scs1Application.class, args);
+	}
+
+}
